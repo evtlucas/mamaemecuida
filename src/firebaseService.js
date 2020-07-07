622db5661fadb8dcdf058bc91a018aecc2de57ad
Everton Lucas <evtlucas@gmail.com>
2020-07-07 07:33:10 -0300

    Fixed analytics initialization

--- src/firebaseService.js
+++ src/firebaseService.js
@@ -3,25 +3,12 @@ import 'firebase/firestore'
 import { config } from './firebaseConfig'
 
 let db = null
-/*
-if (process.env.NODE_ENV === 'development') {
-  const firebaseConfig = {
-    apiKey: config.apiKey,
-    projectId: config.projectId,
-    databaseURL: 'http://localhost:9000?ns=' + config.projectId
-  }
-  const app = firebase.initializeApp(firebaseConfig)
-  app.functions().useFunctionsEmulator('http://localhost:5001')
-  db = firebase.firestore()
-  db.settings({ host: 'localhost:8081', ssl: false })
-} else {
-  firebase.initializeApp(config)
-  db = firebase.firestore()
-}
-*/
 
 firebase.initializeApp(config)
 db = firebase.firestore()
+if (process.env.NODE_ENV === 'production') {
+  firebase.analytics()
+}
 
 // firebase utils
 const auth = firebase.auth()
